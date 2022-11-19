39f777297729f7418dbd42ae0195b2e54b3978c5
ziyomukhammad.usmono <ziyomukhammad.usmonov@geniusto.co.uk>
2022-11-19 15:17:22 +0500

    Initial Commit

---
+++ PGProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PGProject
+//
+//  Created by ZiyoMukhammad Usmonov on 19/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
